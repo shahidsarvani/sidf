788e7567fc0a3e8912b9c1bf04a14391f0e408d8
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2021-09-23 16:34:09 +0500

    Update Dashboard.php

--- cms/controller/Dashboard.php
+++ cms/controller/Dashboard.php
@@ -5,7 +5,7 @@ session_start();
 require './../config/config.php';
 require './../config/database.php';
 
-if(!$_SESSION['user_id']) {
+if(!isset($_SESSION['user_id']) && !$_SESSION['user_id']) {
     header('Location: ' . ADMIN_SITE_URL . '/controller/login.php');
 }
 echo "Dashboard";
