cd086a11d1a0f9de5fc86e879804b9afb6cc7d7a
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2021-12-02 15:43:11 +0500

    Update config.php

--- cms/config/config.php
+++ cms/config/config.php
@@ -1,64 +1,28 @@
 <?php
-$mine = 1;
-if ($_SERVER['HTTP_HOST'] == 'localhost' && $mine == 1) {
-    $con_file_path = __FILE__;
-    $con_file_path = str_replace('\cms\config\config.php', '',  $con_file_path);
-    $con_file_path = str_replace('/cms/config/config.php', '',  $con_file_path);
-    define('BASE_PATH', $con_file_path);
-    //\cms\config\config.php
-
-    // define('BASE_PATH', '/Applications/XAMPP/xamppfiles/htdocs/PHP/sidf');
-    define('BASE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/custom3/sidf');
-    define('ADMIN_SITE_URL', BASE_URL . '/cms');
-    define('ADMIN_ASSET', BASE_URL . '/assets/admin_assets');
-    define('USER_ASSET', BASE_URL . '/assets/frontend_assets');
-    define('ADMIN_VIEW', BASE_PATH . '/cms/views');
-    define('USER_SITE_URL', BASE_URL);
-    $items_config = array(
-        'images_url' => USER_ASSET . '/screen_media/',
-        'images_path' => BASE_PATH . '/assets/frontend_assets/screen_media/',
-        'modal_media_url' => USER_ASSET . '/modal_media/',
-        'modal_media_path' => BASE_PATH . '/assets/frontend_assets/modal_media/',
-    );
-} else /* if ($_SERVER['HTTP_HOST'] == 'localhost' && $mine == 0) */ {
-    $con_file_path = __FILE__;
-    $con_file_path = str_replace('\cms\config\config.php', '',  $con_file_path);
-    $con_file_path = str_replace('/cms/config/config.php', '',  $con_file_path);
-    echo $con_file_path.'<br>';
-    define('BASE_PATH', $con_file_path);
-    $con_file_url = $_SERVER['HTTP_REFERER'];
-    echo json_encode($_SERVER);
-    echo $con_file_url.'<br>';
-    echo strpos($con_file_url,'sidf');
-    $con_file_url = substr($con_file_url, 0, strpos($con_file_url,'sidf')+4);
-    die();
-    define('BASE_URL', $con_file_url);
-    define('ADMIN_SITE_URL', BASE_URL . '/cms');
-    define('ADMIN_ASSET', BASE_URL . '/assets/admin_assets');
-    define('USER_ASSET', BASE_URL . '/assets/frontend_assets');
-    define('ADMIN_VIEW', BASE_PATH . '/cms/views');
-    define('USER_SITE_URL', BASE_URL);
-    $items_config = array(
-        'images_url' => USER_ASSET . '/screen_media/',
-        'images_path' => BASE_PATH . '/assets/frontend_assets/screen_media/',
-        'modal_media_url' => USER_ASSET . '/modal_media/',
-        'modal_media_path' => BASE_PATH . '/assets/frontend_assets/modal_media/',
-    );
-} /* else {
-    define('BASE_PATH', '/var/www/vhosts/digitalpoin8.com//sidf.digitalpoin8.com');
+$mine = 0;
+$host = $_SERVER['HTTP_HOST'];
+$con_file_path = __FILE__;
+$con_file_path = str_replace('\cms\config\config.php', '',  $con_file_path);
+$con_file_path = str_replace('/cms/config/config.php', '',  $con_file_path);
+define('BASE_PATH', $con_file_path);
+if ($host == 'localhost' && $mine == 1) {
+    define('BASE_URL', 'http://' . $host . '/custom3/sidf');
+} else if ($host == 'localhost' && $mine == 0) {
+    define('BASE_URL', 'http://' . $host . '/PHP/sidf');
+} else {
     define('BASE_URL', 'https://sidf.digitalpoin8.com');
-    define('ADMIN_SITE_URL', BASE_URL . '/cms');
-    define('ADMIN_ASSET', BASE_URL . '/assets/admin_assets');
-    define('USER_ASSET', BASE_URL . '/assets/frontend_assets');
-    define('ADMIN_VIEW', BASE_PATH . '/cms/views');
-    define('USER_SITE_URL', BASE_URL);
-    $items_config = array(
-        'images_url' => USER_ASSET . '/screen_media/',
-        'images_path' => BASE_PATH . '/assets/frontend_assets/screen_media/',
-        'modal_media_url' => USER_ASSET . '/modal_media/',
-        'modal_media_path' => BASE_PATH . '/assets/frontend_assets/modal_media/',
-    );
-} */
+}
+define('ADMIN_SITE_URL', BASE_URL . '/cms');
+define('ADMIN_ASSET', BASE_URL . '/assets/admin_assets');
+define('USER_ASSET', BASE_URL . '/assets/frontend_assets');
+define('ADMIN_VIEW', BASE_PATH . '/cms/views');
+define('USER_SITE_URL', BASE_URL);
+$items_config = array(
+    'images_url' => USER_ASSET . '/screen_media/',
+    'images_path' => BASE_PATH . '/assets/frontend_assets/screen_media/',
+    'modal_media_url' => USER_ASSET . '/modal_media/',
+    'modal_media_path' => BASE_PATH . '/assets/frontend_assets/modal_media/',
+);
 
 // $email_config = array(
 //     'email_address' => 'hamza0952454@gmail.com',
@@ -73,20 +37,21 @@ if ($_SERVER['HTTP_HOST'] == 'localhost' && $mine == 1) {
 
 
 /* start of the file uploading function */
-function fileExists($file, $dir) { 		
-	$i=1; 
-	$probeer=$file;
-	while(file_exists($dir.$probeer)) {
-		$punt=strrpos($file,".");
-		if(substr($file,($punt-3),1)!==("[") && substr($file,($punt-1),1)!==("]")) {
-			$probeer=substr($file,0,$punt)."[".$i."]".
-			substr($file,($punt),strlen($file)-$punt);
-		   } else {
-			   $probeer=substr($file,0,($punt-3))."[".$i."]".
-			   substr($file,($punt),strlen($file)-$punt);
-			  }
-			$i++;
-	  }
-	  return $probeer;
+function fileExists($file, $dir)
+{
+    $i = 1;
+    $probeer = $file;
+    while (file_exists($dir . $probeer)) {
+        $punt = strrpos($file, ".");
+        if (substr($file, ($punt - 3), 1) !== ("[") && substr($file, ($punt - 1), 1) !== ("]")) {
+            $probeer = substr($file, 0, $punt) . "[" . $i . "]" .
+                substr($file, ($punt), strlen($file) - $punt);
+        } else {
+            $probeer = substr($file, 0, ($punt - 3)) . "[" . $i . "]" .
+                substr($file, ($punt), strlen($file) - $punt);
+        }
+        $i++;
+    }
+    return $probeer;
 }
 /* end of the file uploading function */
