c9c0c668680ea04d86e546c00eda9db1c287679e
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2021-12-07 17:44:08 +0500

    Update get_json.php

--- cms/controller/companies/get_json.php
+++ cms/controller/companies/get_json.php
@@ -33,8 +33,11 @@ foreach ($companies as $index => $value) {
             $title_ar = $icon["title_ar"];
             $title_eng = $icon["title_eng"];
             $icon_img = $company->get_media($icon["icon"]);
-            $icon_img = $icon_img->fetch_assoc();
-            // $icon_img['name'] = '';
+            if($icon_img) {
+                $icon_img = $icon_img->fetch_assoc();
+            } else {
+                $icon_img['name'] = '';
+            }
             $icon_html .= '<div class="icon_single_inner d-flex flex-column align-items-center">
                             <img class="icon_img" src="' . $items_config['rfid_media_url'] . $icon_img['name'] . '" alt="logo" />
                             <h2 class="text-uppercase arabic-content">' . $title_ar . '</h2>
@@ -42,40 +45,42 @@ foreach ($companies as $index => $value) {
                         </div>';
         }
     }
-    $response['companies'][$token['rfid_card_id']] = '<div class="video_wrapper">
-                <video id="vid" class="w-100 video_inner" autoplay loop muted>
-                    <source src="' . $items_config['rfid_media_url'] . $video['name'] . '" type="' . $video['filetype'] . '">
-                </video>
-            </div>
-            <div class="text_wrapper_outter">
-                <div class="container-fluid h-100">
-                    <div class="row h-100">
-                        <div class="col-6 h-100 d-flex align-items-center justify-content-center left-column">
-                            <div class="images_single_outter w-100">
-                                <div class="images_wrapper_inner d-flex justify-content-center pb-4">
-                                    <img class="w-75" src="' . $items_config['rfid_media_url'] . $logo['name'] . '" alt="logo" />
-                                </div>
-                                <div class="icons_outter d-flex justify-content-around flex-wrap mt-5">
-                                    ' . $icon_html . '
+    if ($token['rfid_card_id']) {
+        $response['companies'][$token['rfid_card_id']] = '<div class="video_wrapper">
+                    <video id="vid" class="w-100 video_inner" autoplay loop muted>
+                        <source src="' . $items_config['rfid_media_url'] . $video['name'] . '" type="' . $video['filetype'] . '">
+                    </video>
+                </div>
+                <div class="text_wrapper_outter">
+                    <div class="container-fluid h-100">
+                        <div class="row h-100">
+                            <div class="col-6 h-100 d-flex align-items-center justify-content-center left-column">
+                                <div class="images_single_outter w-100">
+                                    <div class="images_wrapper_inner d-flex justify-content-center pb-4">
+                                        <img src="' . $items_config['rfid_media_url'] . $logo['name'] . '" alt="logo" />
+                                    </div>
+                                    <div class="icons_outter d-flex justify-content-around flex-wrap mt-5">
+                                        ' . $icon_html . '
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    
-                        <div class="col-6 h-100 d-flex align-items-center justify-content-center flex-column right-column">
-                            <div class="scroll-bar">
-                                <div class="text_wrapper_op p-4 direction_rtl mb-4 arabic-content">
-                                    <h1 class="text-uppercase heading_main">' . $value['name_ar'] . '</h1>
-                                    <p>' . $value['info_ar'] . '</p>
-                                </div>
-                                <div class="text_wrapper_op p-4">
-                                    <h1 class="text-uppercase heading_main">' . $value['name_eng'] . '</h1>
-                                    <p>' . $value['info_eng'] . '</p>
+                        
+                            <div class="col-6 h-100 d-flex align-items-center justify-content-center flex-column right-column">
+                                <div class="scroll-bar">
+                                    <div class="text_wrapper_op p-4 direction_rtl mb-4 arabic-content">
+                                        <h1 class="text-uppercase heading_main">' . $value['name_ar'] . '</h1>
+                                        <p>' . $value['info_ar'] . '</p>
+                                    </div>
+                                    <div class="text_wrapper_op p-4">
+                                        <h1 class="text-uppercase heading_main">' . $value['name_eng'] . '</h1>
+                                        <p>' . $value['info_eng'] . '</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>';
+                </div>';
+    }
 }
 // echo json_encode($response);
 // die();
